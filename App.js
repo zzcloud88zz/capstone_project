e7b8cec41e4cc300ea555182dc80243a87690dbb
shengrong <shengrong_88@hotmail.com>
2021-02-16 11:14:23 +0800

    added 1) stack nav 2) home & barcodescanner screen (blank screen)

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./Screens/HomeScreen";
+import BarcodeScannerScreen from "./Screens/BarcodeScannerScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Barcode Scanner" component={BarcodeScannerScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
